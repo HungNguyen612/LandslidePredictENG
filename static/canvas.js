f10584f001525146b5b6b57571c5e619c9e0f9fa
HungNguyen612 <hung049005@stu.vinschool.edu.vn>
2025-01-22 15:05:22 +0700

    Update canvas.js

--- static/canvas.js
+++ static/canvas.js
@@ -1,17 +1,39 @@
 
         const canvas = document.getElementById('myCanvas');
         const ctx = canvas.getContext('2d'); // Get the 2D drawing context
-        let radius1 = 50
-        let radius2 = 40
+        const width = canvas.width
+        let radius1 = 100
+        let radius2 = 90
+        let space = width/7
+        let x = space
+        let y = 200
+        
+        function circle() {
         // Draw a circle
         ctx.beginPath();
-        ctx.arc(600, 200, radius1, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
+        ctx.arc(x, y, radius1, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
         ctx.fillStyle = 'red';
         ctx.fill();
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.arc(600, 200, radius2, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
+        ctx.arc(x, y, radius2, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
         ctx.fillStyle = 'blue';
         ctx.fill();
         ctx.stroke();
+        }
+        circle()
+        x += space
+        circle()
+        x += space
+        circle()
+        x += space
+        circle()
+        x += space
+        circle()
+        x += space
+        circle()
+        x += space
+        circle()
+        x += space
+        
